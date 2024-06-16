9533008cf16d032027a3e52f4b0716dda3fe436e
Gabriel Pichot <gabriel.pichot@gmail.com>
2024-06-16 23:01:26 +0200

    feat: add slides for meetup 002

---
+++ src/002-2024-06-18/data.ts
@@ -0,0 +1,56 @@
+const communityMeetups = [
+  {
+    groupName: "Vue.js Paris",
+    title: "Vue.js Paris #26",
+    date: "Mardi 25 Juin",
+    location: "Valtech",
+  },
+];
+
+const talks = [
+  {
+    title: "Let's re-implement useState in VanillaJS !",
+    description: `Wanna know how hooks work under the hood?
+
+Wanna know why the Rules of Hooks bother you?
+
+This talk is for you!
+
+We will re-implement useState hook in vanilla JavaScript`,
+    speaker: {
+      name: "Pierre Nicoletti",
+      title: "Full Stack Engineer & Team Leader @In The Memory",
+      profilePicture:
+        "https://media.licdn.com/dms/image/C4D03AQHKDwqyW_HI-w/profile-displayphoto-shrink_800_800/0/1574187753331?e=1722470400&v=beta&t=MupxR50xq_PjCM3uYk8z9HNq25OqTYLX3vBloYDFPW0",
+    },
+  },
+  {
+    title: "Les nouveautés de React 19 et comment les utiliser ?",
+    description: `React est sur le point de ça version 19 en version officielle.
+
+Ce talk présente les nouvelles fonctionnalités de la toute dernière version de React, comment les utiliser et les intégrer dans une code base.
+
+Avec entre autre les nouveautés compilateur React pour essayer de comprendre où va React avec ces nouveautés. 
+`,
+    speaker: {
+      name: "Clément Chalopin",
+      title: "Développeur Full Stack @3DS OUTSCALE",
+      profilePicture:
+        "https://media.licdn.com/dms/image/D5603AQE9aaYsck4Cbw/profile-displayphoto-shrink_800_800/0/1701955831644?e=1722470400&v=beta&t=nDmE-TT09LDxkJP1dkVEtFfoXpC78AECtRXMeK65tYc",
+    },
+  },
+  {
+    title: "React + ThreeJS = React Three Fiber",
+    description: `Le but de ce talk serait de présenter rapidement à travers une petite démo comment utiliser ThreeJS (pour faire de la 3d) en React avec la lib react-three-fiber.
+`,
+
+    speaker: {
+      name: "Jeremie Taboada",
+      title: "Développeur Freelance & Co-fondateur Code Passport",
+      profilePicture:
+        "https://media.licdn.com/dms/image/C4D03AQHU1vdjlAR7ig/profile-displayphoto-shrink_800_800/0/1545126965804?e=1724284800&v=beta&t=t_XRygz8hOkhFo5UOUkYfbaujOQMBY75Ma8ZWlzFE7s",
+    },
+  },
+];
+
+export { communityMeetups, talks };
